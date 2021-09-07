c2685c5b077cc83bb3338e06a37d516a784055e2
sivert <233518@usn.no>
2021-09-07 22:16:50 +0200

    Glemte scene

--- app/src/main/java/com/example/happyhike/scenes/KartScene.kt
+++ app/src/main/java/com/example/happyhike/scenes/KartScene.kt
@@ -1,10 +1,9 @@
 package com.example.happyhike.scenes
 
+import OnMapReadyCallbackcom.example.happyhike.controllers.KartController
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.happyhike.R
-import com.example.happyhike.controllers.KartController
-import com.google.android.gms.maps.SupportMapFragment
 
 class KartScene : AppCompatActivity() {
     //Variables
@@ -13,8 +12,6 @@ class KartScene : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
         kartController = KartController(this)
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
     }
 
 }
