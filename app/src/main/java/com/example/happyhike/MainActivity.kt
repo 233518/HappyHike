cd670f67d74e0f0ad48a8465fc41d7b0eead5b55
sivert <233518@usn.no>
2021-09-07 23:21:44 +0200

    Logging

--- app/src/main/java/com/example/happyhike/MainActivity.kt
+++ app/src/main/java/com/example/happyhike/MainActivity.kt
@@ -2,12 +2,7 @@ package com.example.happyhike
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import android.content.Intent
 import com.example.happyhike.scenes.KartScene
 
--- app/src/main/java/com/example/happyhike/logger/Logger.kt
+++ app/src/main/java/com/example/happyhike/logger/Logger.kt
@@ -1,4 +1,35 @@
 package com.example.happyhike.logger
 
-class Logger {
+import android.os.Build
+import android.util.Log
+import com.example.happyhike.BuildConfig
+import java.time.LocalDateTime
+
+open class Logger {
+    companion object {
+        open fun debug(message: String) {
+            if (BuildConfig.DEBUG) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    Log.d("Debug","[DEBUG][" + LocalDateTime.now() + "]: " + message)
+                }
+            }
+        }
+        open fun error(message: String) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                Log.d("Error","[ERROR][" + LocalDateTime.now() + "]: " + message)
+            }
+        }
+
+        open fun warning(message: String) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                Log.d("WARNING","[WARNING][" + LocalDateTime.now() + "]: " + message)
+            }
+        }
+
+        open fun info(message: String) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                Log.d("INFO","[INFO][" + LocalDateTime.now() + "]: " + message)
+            }
+        }
+    }
 }
