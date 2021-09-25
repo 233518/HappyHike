73667124b8e3f7df5cf8ef0613d0f9d3de6a2340
dyb0 <dybevik2@gmail.com>
2021-09-25 22:22:23 +0200

    oppdatert login og registering activities

--- app/src/main/java/com/example/filmatory/scenes/LoginScene.kt
+++ app/src/main/java/com/example/filmatory/scenes/LoginScene.kt
@@ -1,7 +1,9 @@
 package com.example.filmatory.scenes
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -16,12 +18,21 @@ import com.google.android.material.navigation.NavigationView
 class LoginScene : AppCompatActivity() {
     private var apiSystem = ApiSystem()
     private lateinit var navSystem: NavSystem
+    private lateinit var regBtn: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         apiSystem.requestApprovedReviewById("60b3a9194001540015069d2c", ::printTest)
         setContentView(R.layout.login_screen)
         navSystem = NavSystem(this)
+        regBtn = findViewById(R.id.regHereBtn)
+
+        regBtn.setOnClickListener {
+            val intent = Intent(this, RegisterScene::class.java)
+            startActivity(intent)
+        }
     }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(navSystem.toggle.onOptionsItemSelected(item)){
             return true
--- app/src/main/java/com/example/filmatory/scenes/RegisterScene.kt
+++ app/src/main/java/com/example/filmatory/scenes/RegisterScene.kt
@@ -1,12 +1,22 @@
 package com.example.filmatory.scenes
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.filmatory.R
+import com.example.filmatory.systems.NavSystem
 
 class RegisterScene : AppCompatActivity() {
+    private lateinit var navSystem: NavSystem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_screen)
+        navSystem = NavSystem(this)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(navSystem.toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
