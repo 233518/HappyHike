2074e2f83f54a636eb64b7d7b95a67d980047122
dyb0 <50889290+dyb0@users.noreply.github.com>
2021-10-24 01:51:15 +0200

    Lister og liste + nytt Movie data class for å få skuespillere også

---
+++ app/src/main/java/com/example/filmatory/api/data/movie/Cast.kt
@@ -0,0 +1,7 @@
+package com.example.filmatory.api.data.movie
+
+data class Cast(
+    val cast: List<CastX>,
+    val crew: List<Crew>,
+    val id: Int
+)
--- app/src/main/java/com/example/filmatory/controllers/sceneControllers/MovieController.kt
+++ app/src/main/java/com/example/filmatory/controllers/sceneControllers/MovieController.kt
@@ -19,16 +19,16 @@ class MovieController(movieScene: MovieScene) : MainController(movieScene) {
     fun getMovie(movie: Movie){
         movieScene.runOnUiThread(Runnable {
             println(movie)
-            movieScene.findViewById<TextView>(R.id.m_title).text = movie.title
-            movieScene.findViewById<TextView>(R.id.m_date).text = movie.release_date
+            movieScene.findViewById<TextView>(R.id.m_title).text = movie.filminfo.title
+            movieScene.findViewById<TextView>(R.id.m_date).text = movie.filminfo.release_date
             Glide.with(movieScene)
-                .load("https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + movie.poster_path)
+                .load("https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + movie.filminfo.poster_path)
                 .placeholder(R.drawable.placeholder_image)
                 .fallback(R.drawable.placeholder_image)
                 .error(R.drawable.placeholder_image)
                 .centerCrop()
                 .into(movieScene.findViewById(R.id.m_img))
-            movieScene.findViewById<TextView>(R.id.m_overview).text = movie.overview
+            movieScene.findViewById<TextView>(R.id.m_overview).text = movie.filminfo.overview
         })
     }
 }
