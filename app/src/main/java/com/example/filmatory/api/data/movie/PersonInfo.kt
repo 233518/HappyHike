bd75991e7300a3ff96abaae125a834f1957f55aa
Sigve E. Eliassen <sigve888@gmail.com>
2021-10-23 17:17:01 +0200

    Movie Personslider API data

---
+++ app/src/main/java/com/example/filmatory/api/data/movie/PersonInfo.kt
@@ -0,0 +1,6 @@
+package com.example.filmatory.api.data.movie
+
+data class PersonInfo(
+    val persons: Persons,
+    val movie: Movie
+)
---
+++ app/src/main/java/com/example/filmatory/api/data/movie/Persons.kt
@@ -0,0 +1,7 @@
+package com.example.filmatory.api.data.movie
+
+data class Persons(
+    val cast: List<Cast>,
+    val crew: List<Crew>,
+    val id: Int
+)
