be092dd78a6fd1d1837f49d50d20d1c144f66f65
sivert <sivert.b.heisholt@protonmail.com>
2021-11-05 15:46:23 +0100

    Revert "Success/error message on movie/tv"
    This reverts commit 149dccce

--- app/src/main/java/com/example/filmatory/systems/SnackbarSystem.kt
+++ app/src/main/java/com/example/filmatory/systems/SnackbarSystem.kt
@@ -16,12 +16,13 @@ class SnackbarSystem(private var view : View) {
 
     //Custom
     private var length = Snackbar.LENGTH_LONG
-    var duration = 5000
+        set(value) {field = value}
+    private var duration = 5000
         set(value) {field = value}
     private var messageTextColor = context.getColor(R.color.white)
 
     data class ActionSnack(
-        val function: Any,
+        val function: () -> Any,
         val actionMsg: String,
         val actionMsgColor: Int,
     )
@@ -45,10 +46,10 @@ class SnackbarSystem(private var view : View) {
      * @param actionMsg The message of the
      * @author Sivert Heisholt - 233518
      */
-    fun showSnackbarFailure(message: String, function: () -> Unit, actionMsg: String) {
-        val colorBackground = context.getColor(R.color.snackbar_failure)
-        val colorActionMsg = context.getColor(R.color.white)
-        val actionSnack = ActionSnack(function, actionMsg, colorActionMsg)
+    fun showSnackbarFailure(message: String, function: () -> Any, actionMsg: String) {
+        var colorBackground = context.getColor(R.color.snackbar_failure)
+        var colorActionMsg = context.getColor(R.color.white)
+        var actionSnack = ActionSnack(function, actionMsg, colorActionMsg)
 
         makeSnackbar(message, colorBackground, actionSnack)
     }
@@ -71,7 +72,7 @@ class SnackbarSystem(private var view : View) {
      * @author Sivert Heisholt - 233518
      */
     fun showSnackbarWarning(message: String) {
-        val color = context.getColor(R.color.snackbar_warning)
+        var color = context.getColor(R.color.snackbar_warning)
         makeSnackbar(message, color, null)
     }
 
@@ -84,10 +85,14 @@ class SnackbarSystem(private var view : View) {
      * @author Sivert Heisholt - 233518
      */
     private fun makeSnackbar(message: String, color: Int, actionSnack: ActionSnack?){
-        val snackbar = Snackbar.make(view, message, length)
-            .setAction(actionSnack?.actionMsg) {
-                actionSnack?.function
-            }.setActionTextColor(actionSnack!!.actionMsgColor)
+        var snackbar = Snackbar.make(view, message, length)
+
+        if(actionSnack != null) {
+            snackbar.setAction(actionSnack.actionMsg) {
+                actionSnack.function
+            }
+            snackbar.setActionTextColor(actionSnack.actionMsgColor)
+        }
 
         snackbar.duration = duration
         snackbar.view.setBackgroundColor(color)
