89ff8a351bafab9db980a642536bb6f20226a2e0
sivertheisholt <sivert.b.heisholt@protonmail.com>
2021-12-03 21:45:51 +0100

    Removed User

--- app/src/main/java/com/example/filmatory/entities/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.filmatory.entities
-
-class User(email : String, password : String, username : String) {
-    var email = email
-    var password = password
-    var username = username
-    
-}
